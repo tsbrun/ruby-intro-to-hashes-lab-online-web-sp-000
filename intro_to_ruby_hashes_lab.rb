f93c925c4205162570543a8b309ae68a520f0e9d
Anya Brun <tsbrun@icloud.com>
2020-07-29 18:39:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def new_hash
   new_hash = {}
 end
@@ -11,6 +13,7 @@ end
 def monopoly
 	monopoly = {}
   monopoly[:railroads] = new_hash
+  binding.pry
 end
 
 def monopoly_with_second_tier
