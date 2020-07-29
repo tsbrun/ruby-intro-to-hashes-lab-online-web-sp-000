e910ef7a3ef2a08c309d30dcc8728d33c274db0b
Anya Brun <tsbrun@icloud.com>
2020-07-29 18:48:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,6 +42,7 @@ def monopoly_with_third_tier
 
   monopoly[:railroads][:names][:reading_railroad] = {}
   monopoly[:railroads][:names][:pennsylvania_railroad] = {}
+  monopoly[:railroads][:names][:b_and_o_railroad] = {}
 
   monopoly
 end
